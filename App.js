0d34f118b01dc63219c813d8dabb2d70671a0099
lags2022 <lguzman.58erb@outlook.com>
2023-04-25 16:23:11 -0500

    update stripe

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { NativeRouter } from "react-router-native";
 import { StatusBar } from "expo-status-bar";
 import store from "./src/redux/store";
 import { Provider } from "react-redux";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <StatusBar style="light" />
-      <NativeRouter>
-        <Main />
-      </NativeRouter>
-    </Provider>
+    <StripeProvider publishableKey="pk_test_51My0xcEfYxNbjr9ynwLm1ZMXJa20yzHU9FTVr8SxVzEpHVEHdsvsq7HoAsaayV9EkkqYGZ68a18vnYuAuEiZlVva00dqsr0OA2">
+      <Provider store={store}>
+        <StatusBar style="light" />
+        <NativeRouter>
+          <Main />
+        </NativeRouter>
+      </Provider>
+    </StripeProvider>
   );
 }
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,24 +1,92 @@
-import { Text, FlatList, Button } from "react-native";
+import { Text, FlatList, Button, Alert } from "react-native";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectItems } from "../redux/slice/basketSlice";
+import { selectItems, selectTotal } from "../redux/slice/basketSlice";
 import { useDispatch } from "react-redux";
 import { removeFromBasket } from "../redux/slice/basketSlice";
 import RepositoryItem from "./RepositoryItem";
 import auth from "@react-native-firebase/auth";
+import { useStripe } from "@stripe/stripe-react-native";
 
 function Checkout() {
   const dispatch = useDispatch();
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
   const [autenticated, setAutenticated] = useState(false);
+  const [user, setUser] = useState(null);
 
-  const createCheckoutSession = () => {
-    console.log("comprastes");
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [loading, setLoading] = useState(false);
+
+  const fetchPaymentSheetParams = async () => {
+    try {
+      const response = await fetch(
+        "https://amazon-clone-lags2022.vercel.app/api/rncreate-checkout-session",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            total,
+            items,
+            email: user.email,
+          }),
+        }
+      );
+      const { paymentIntent, ephemeralKey, customer } = await response.json();
+
+      return {
+        paymentIntent,
+        ephemeralKey,
+        customer,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { paymentIntent, ephemeralKey, customer } =
+      await fetchPaymentSheetParams();
+
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      paymentIntentClientSecret: paymentIntent,
+      // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+      //methods that complete payment after a delay, like SEPA Debit and Sofort.
+      allowsDelayedPaymentMethods: false,
+      defaultBillingDetails: {
+        name: "Jane Doe",
+      },
+    });
+    if (!error) {
+      setLoading(true);
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      Alert.alert("Success", "Your order is confirmed!");
+    }
   };
 
   useEffect(() => {
+    user && initializePaymentSheet();
+  }, [user]);
+
+  useEffect(() => {
     const subscriber = auth().onAuthStateChanged((user) => {
-      user ? setAutenticated(true) : setAutenticated(false);
+      if (user) {
+        setUser(user);
+        setAutenticated(true);
+      } else setAutenticated(false);
     });
     return subscriber;
   }, []);
@@ -40,9 +108,9 @@ function Checkout() {
         )}
       />
       <Button
-        disabled={!autenticated}
-        onPress={createCheckoutSession}
+        disabled={!autenticated || !loading}
         title={!autenticated ? "Sign in to Checkout" : "Proceed to Checkout"}
+        onPress={openPaymentSheet}
       />
     </>
   );
